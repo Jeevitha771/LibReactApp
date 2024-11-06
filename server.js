f1b585d57087631a554f9571b27f733ce6245ffd
Jeevitha771 <81794463+Jeevitha771@users.noreply.github.com>
2024-11-06 15:14:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT1 || 7210;
+const PORT = process.env.PORT1 || 5000;
 
 // Middleware
 app.use(cors());
